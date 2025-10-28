56703e6a4df6f89d24acdfda70b68b014e1a220d
lziolek <lubomirziolek>
2025-10-28 15:54:35 +0100

    test

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -5,4 +5,25 @@
 class ResourceManager
 {
     // Twoja implementacja tutaj
+    Resource* resource_;
+public:
+    ResourceManager(): resource_(new Resource()) {};
+
+    ResourceManager(const ResourceManager& other)
+        : resource_{other.resource_}{}
+
+    ResourceManager& operator=(const ResourceManager& other)
+    {
+        if (this == &other) return *this;
+        Resource* new_resource = new Resource(*other.resource_);
+        delete resource_;
+        resource_ = new_resource;
+        return *this;
+    }
+
+
+    double get() { return resource_->get(); }
+
+    ~ResourceManager() {delete resource_; }
+
 };
